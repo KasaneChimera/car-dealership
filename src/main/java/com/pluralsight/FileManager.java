0f744de3a1e4f9b9a01c6f3da4850c062bb12a93
KasaneChimera <sxayala15@email.com>
2026-05-08 13:36:14 -0400

    whoops forgot to commit

--- src/main/java/com/pluralsight/FileManager.java
+++ src/main/java/com/pluralsight/FileManager.java
@@ -1,4 +1,72 @@
 package com.pluralsight;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileManager {
+
+    public Dealership getDealership() {
+
+        Dealership dealership = null;
+
+        try {
+
+            BufferedReader br =
+                    new BufferedReader(
+                            new FileReader("inventory.csv"));
+
+            String firstLine = br.readLine();
+
+            String[] dealershipParts =
+                    firstLine.split("\\|");
+
+            String name = dealershipParts[0];
+            String address = dealershipParts[1];
+            String phone = dealershipParts[2];
+
+            dealership =
+                    new Dealership(name, address, phone);
+
+            String line;
+
+            while ((line = br.readLine()) != null) {
+
+                String[] parts = line.split("\\|");
+
+                int vin = Integer.parseInt(parts[0]);
+                int year = Integer.parseInt(parts[1]);
+                String make = parts[2];
+                String model = parts[3];
+                String vehicleType = parts[4];
+                String color = parts[5];
+                int odometer = Integer.parseInt(parts[6]);
+                double price = Double.parseDouble(parts[7]);
+
+                Vehicle vehicle =
+                        new Vehicle(
+                                vin,
+                                year,
+                                make,
+                                model,
+                                vehicleType,
+                                color,
+                                odometer,
+                                price
+                        );
+
+                dealership.addVehicle(vehicle);
+            }
+
+            br.close();
+
+        } catch (IOException e) {
+            System.out.println("Error reading file.");
+        }
+
+        return dealership;
+    }
+
+    public void saveDealership(Dealership dealership) {
+
+    }
 }
--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,4 +1,17 @@
 package com.pluralsight;
 
 public class Program {
+
+    public static void main(String[] args) {
+
+        FileManager fileManager =
+                new FileManager();
+
+        Dealership dealership =
+                fileManager.getDealership();
+
+        for (Vehicle vehicle : dealership.getAllVehicles()) {
+            System.out.println(vehicle);
+        }
+    }
 }
